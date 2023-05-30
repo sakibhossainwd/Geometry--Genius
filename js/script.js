39f1f999b933b7fa9fe8be8be0ff910cf028e3d3
sakibhossain <sakibhossainceo@gmail.com>
2023-05-29 20:34:07 -0700

    alert added

--- js/script.js
+++ js/script.js
@@ -25,14 +25,21 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     setCalculationResul('triangle-result', multipleTotalArea);
 
     // show are result
-    const list = document.querySelector('.triangle-none')
-    console.log(list);
-    list.classList.remove("triangle-none");
+    // const list = document.querySelector('.triangle-none')
+    // console.log(list);
+    // list.classList.remove("triangle-none");
     
 })
 
 // Triangle calculation "convert to m2" button
 document.getElementById('btn-convert').addEventListener('click', function(){
+    // alert
+    const triangleResult = document.getElementById('triangle-result');
+    const triangleResultText = triangleResult.innerText;
+    if(triangleResultText === '00'){
+        alert('please input valid number');
+        return;
+    }
     const trianglePreviousNumber = getElementValueById('triangle-pre-number');
     const triangleInputLeft = inputField('triangle-left');
     const triangleInputRight = inputField('triangle-right');
@@ -41,6 +48,10 @@ document.getElementById('btn-convert').addEventListener('click', function(){
     const convertCmToM2 = multipleTotalArea / 10;
     const convertResult = convertCmToM2.toFixed(2);
     setCalculationResul('triangle-result', convertResult);
+    
+    // // change the btn text 
+    // const btnText = document.getElementById('triangle-convert-cm');
+    // btnText.innerText = 'convert to cm';
 })
 
 // Ractabgle part
